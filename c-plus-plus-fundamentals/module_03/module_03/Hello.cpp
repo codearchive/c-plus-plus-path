53453d8939a1e3d8359f17a840bb1f703b9c6115
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-16 23:58:50 +0300

    Completed unit 3.7 - Demo: Visual Studio Hello World

---
+++ c-plus-plus-fundamentals/module_03/module_03/Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	using namespace std;
+	cout << "Type your name" << endl;
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << endl;
+
+	return 0;
+}
