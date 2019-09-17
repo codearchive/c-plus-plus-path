bdf376d44f867024abef98600c875df5d82066cc
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-17 17:22:01 +0300

    Completed unit 4.5 - Demo: Fundamental Types

--- c-plus-plus-fundamentals/module_03/module_03/Hello.cpp
+++ c-plus-plus-fundamentals/module_03/module_03/Hello.cpp
@@ -4,10 +4,63 @@
 int main()
 {
 	using namespace std;
-	cout << "Type your name" << endl;
-	string name;
-	cin >> name;
-	cout << "Hello, " << name << endl;
+
+	int i1 = 1;
+	cout << "i1 = " << i1 << endl;
+
+	int i2;
+	i2 = 2;
+	cout << "i2 = " << i2 << endl;
+
+	int i3(3);
+	cout << "i3 = " << i3 << endl;
+
+	int i4{ 4 };
+	cout << "i4 = " << i4 << endl;
+
+	double d1 = 2.2;
+	double d2 = i1;
+	int i5 = d1;
+	cout << "d1 = " << d1 << endl;
+	cout << "d2 = " << d2 << endl;
+	cout << "i5 = " << i5 << endl;
+
+	char c1 = 'a';
+	//char c2 = 'b';
+	cout << "c1 = " << c1 << endl;
+	//cout << "c2= " << c2 << endl;
+
+	bool flag = false;
+	cout << "flag = " << flag << endl;
+	flag = i1;
+	cout << "flag = " << flag << endl;
+	flag = d1;
+	cout << "flag = " << flag << endl;
+
+	auto a1 = 1;
+	auto a2 = 2.2;
+	auto a3 = 'c';
+	auto a4 = "s";
+	auto a5 = true;
+	auto a6 = 3L;
+	auto a7 = 1'000'000'000'000;
+	auto a8 = 0xFF;
+	auto a9 = 0b111;
+
+	a1 = a2;
+
+	unsigned char n1 = 128;
+	char n2 = 128;
+	cout << "n1 = " << n1 << endl;
+	cout << "n2 = " << n2 << endl;
+	n1 = 254;
+	n2 = 254;
+	cout << "n1 = " << n1 << endl;
+	cout << "n2 = " << n2 << endl;
+	n1 = 300;
+	n2 = 300;
+	cout << "n1 = " << n1 << endl;
+	cout << "n2 = " << n2 << endl;
 
 	return 0;
 }
